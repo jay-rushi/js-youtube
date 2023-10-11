186dcbb7c8897ed6eac6c49a6f04518ee3aaa0b9
jay-rushi <128637701+jay-rushi@users.noreply.github.com>
2023-10-11 12:53:01 +0530

    Create 04_comparsion.js

---
+++ 01_basics/04_comparsion.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); //(values comes true because = and <> comparsion signs work differently in comparsion null value changes) 
+
+console.log(undefined == 0); 
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===  (in === this the data type gets checked and the value doesnt give true becuase"2" is string data type and 2 is int data type)
+
+console.log("2" === 2);
