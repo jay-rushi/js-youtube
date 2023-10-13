5b91b63ba708d42c3293e59f6740655b5027d293
jay-rushi <128637701+jay-rushi@users.noreply.github.com>
2023-10-13 15:33:35 +0530

    Create 02_scope.js

---
+++ 02_scope.js
@@ -0,0 +1,58 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+//{}    //* this is called as scope*
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1                                     //*method 1 to decalre a function*
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){                           //* method 2 to declare a function here addtwo will give error because it is outside the variable stored value* 
+    return num + 2                                      //* there are variety of methods to declare functions this is called as hosting*
+}
